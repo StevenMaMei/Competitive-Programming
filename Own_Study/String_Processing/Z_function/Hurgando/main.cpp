fe6b6a4a3c90b6c86da2054f26a41980cc4495c6
StevenMaMei <33093117+StevenMaMei@users.noreply.github.com>
2019-10-18 19:17:01 -0500

    empezando hurgando

--- Own_Study/String_Processing/Z_function/Hurgando/main.cpp
+++ Own_Study/String_Processing/Z_function/Hurgando/main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
+
+vector<int> z_function(string s)
+{
+    int n = (int) s.length();
+    vector<int> z(n);
+    for(int i = 1, l = 0, r = 0; i < n; ++i)
+    {
+        if(i <= r)
+            z[i] = min (r - i + 1, z[i - l]);
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]])
+            ++z[i];
+        if(i + z[i] - 1 > r)
+            l= i, r = i + z[i] -1;
+    }
+    return z;
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int n,r,m;
+    string t;
+    cin >> n;
+    cin >> t;
+    cin>> r;
+    cin>> m;
+    for(int r1 = 0; r1 < r; r1++){
+        string s;
+        cin >> s;
+    }
     return 0;
 }
